3725c97232073cd36e9e0b7ee62023e2ed6c6a9f
Mayur8182 <mkbharvad8080@gmail.com>
2025-07-01 20:18:56 +0530

    first commit

---
+++ fire/static/service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'fire-shakti-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/style.css', // Assuming you have a main CSS file
+  '/static/js/main.js',   // Assuming you have a main JS file
+  '/static/icons/icon-192x192.png',
+  '/static/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
